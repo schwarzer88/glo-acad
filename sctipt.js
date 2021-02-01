df59d48801a50b4003f568ea2b2214b6ab938469
Maxim Abrosimov <maxim.abro@yandex.ru>
2021-02-01 11:36:16 +0300

    Третий урок

--- sctipt.js
+++ sctipt.js
@@ -17,3 +17,37 @@ console.log(addExpenses.toLowerCase().split(','));
 
 let budgetDay = money / 30;
 console.log(budgetDay);
+
+//Урок 3
+
+money = +prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+expenses1 = prompt('Введите обязательную статью расходов?');
+amount1 = +prompt('Во сколько это обойдется?', '100');
+expenses2 = prompt('Введите обязательную статью расходов?');
+amount2 = +prompt('Во сколько это обойдется?', '100');
+
+
+//Высчитываем месячный бюджет
+let budgetMonth = +money - (amount1 + amount2);
+console.log('бюджет за месяц ' + budgetMonth);
+
+
+//высчитываем, сколько месяцев надо, чтобы достигнуть цели.
+console.log('за ' + Math.ceil(mission / budgetMonth) + ' месяцев');
+
+budgetDay = Math.floor(budgetMonth / 30);
+console.log('бюджет в день ' + budgetDay);
+
+if (budgetDay >= 1200) {
+    console.log('У вас высокий доход');
+} else if (budgetDay >= 600) {
+    console.log('У вас средний доход');
+} else if (budgetDay >= 0) {
+    console.log('К сожалению у вас доход ниже среднего');
+} else {
+    console.log('Что то пошло не так');
+}
