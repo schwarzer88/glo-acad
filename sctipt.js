ebaabfad1b71e616258cee6d71da836c8e39733d
Maxim Abrosimov <maxim.abro@yandex.ru>
2021-02-04 15:44:18 +0300

    с рандомом

--- sctipt.js
+++ sctipt.js
@@ -1,21 +1,26 @@
 "use strict"
 
-function game(x) {
-    let y = prompt('Угадай число от 1 до 100');
-    if (y === 'undefined' || y == null || y === 0) {
-        alert('Игра окончена');
-    } else if (!parseInt(y)) {
-        alert('Введите число!!');
-        game(x);
-    } else if (y < x) {
-        alert('Загаданное число больше');
-        game(x);
-    } else if (y > x) {
-        alert('Загаданное число меньше');
-        game(x);
-    } else if (y == x) {
-        alert('Поздравляю, Вы угадали');
+function game() {
+    let x = Math.floor(Math.random() * (Math.ceil(100) - Math.floor(1))) + Math.floor(1);
+
+    function startGame(x) {
+        let y = prompt('Угадай число от 1 до 100');
+        if (y === 'undefined' || y == null || y === 0) {
+            alert('Игра окончена');
+        } else if (!parseInt(y)) {
+            alert('Введите число!!');
+            startGame(x);
+        } else if (y < x) {
+            alert('Загаданное число больше');
+            startGame(x);
+        } else if (y > x) {
+            alert('Загаданное число меньше');
+            startGame(x);
+        } else if (y == x) {
+            alert('Поздравляю, Вы угадали');
+        }
     }
+    startGame(x);
 }
 
-game(50);
+game();
