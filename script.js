9ed3f4e6445de9234ba1d7b29676e0d56cb934fa
Maxim Abrosimov <maxim.abro@yandex.ru>
2021-02-09 13:26:00 +0300

    v1

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 "use strict";
-console.log(parseInt('таксую'));
+
+const elemStart = document.getElementById('start');
+const incomePlus = document.querySelector('.income > .income_add');
+const expenses = document.querySelector('.expenses > .expenses_add');
+const depositCheck = document.querySelector('#deposit-check');
+const additionalIncome = document.querySelectorAll('.additional_income-item');
+//выводы
+const budgetMonthValue = document.getElementsByClassName('budget_month-value');
+const budgetDayValue = document.getElementsByClassName('budget_day-value');
+const expensesMonthValue = document.getElementsByClassName('expenses_month-value');
+const additionalIncomeValue = document.getElementsByClassName('additional_income-value');
+const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+const incomePeriodValue = document.getElementsByClassName('income_period-value');
+const targetMonthValue = document.getElementsByClassName('target+month-value');
+//вводы
+const salaryAmount = document.querySelector('.salary-amount');
+const incomeTitle = document.querySelector('.income-title');
+const incomeAmount = document.querySelector('.income-amount');
+const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const expensesTitle = document.querySelector('.expenses-items > .expenses-title');
+const expensesAmount = document.querySelector('.expenses-amount');
+const additionalExpensesIten = document.querySelector('.additional_expenses-item');
+const depositAmount = document.querySelector('.deposit-amount');
+const depositPercent = document.querySelector('.deposit-percent');
+const targetAmount = document.querySelector('.target-amount');
+const periodSelect = document.querySelector('.period-select');
+
+
 let isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
