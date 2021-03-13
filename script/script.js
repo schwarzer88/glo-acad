951848084abdebbc60b160259edcddcf519bd43b
Maxim Abrosimov <maxim.abro@yandex.ru>
2021-03-13 14:33:10 +0300

    v2

--- script/script.js
+++ script/script.js
@@ -73,6 +73,13 @@ window.addEventListener('DOMContentLoaded', function() {
             } else {
                 target = e.target.closest('a');
                 if (target) {
+                    e.preventDefault();
+                    const blockId = target.hash;
+                    console.dir(target.hash);
+                    document.querySelector(blockId).scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    })
                     handlerMenu();
                 } else {
                     return;
